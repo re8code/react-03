4e1a3d2d2beb307e1799527c68fe971578e98ba8
re8code <won@re8code.com>
2024-10-16 22:32:47 +0900

    final, clear

--- src/App.js
+++ src/App.js
@@ -5,10 +5,14 @@ function App() {
   return (
     <>
       <div className='base'>
-        <Basic type='1' data='/right1.png' />
-        <Basic type='1' data='/right2.png' />
+        <Basic type='text' data='1st Paragraph' />
+        <Basic type='image' path='/right1.png' />
+        <Basic type='image' path='/right2.png' />
       </div>
       <div className='base'>
+        <Basic type='text' data='2nd Paragraph' />
+        <Basic type='card' path='/icon1.png' data='a set of printed sheets of paper that are held together inside a cover, a long written work' />
+        <Basic type='card' path='/icon2.png' data='the organ of the body in the head that controls functions, movements, sensations, and thoughts' />
       </div>
     </>
   );
--- src/Basic.js
+++ src/Basic.js
@@ -1,9 +1,24 @@
 import './App.css';
 
 function element (props) {
-
-  if (props.type == '1') {
-    return <img src={props.data} alt='image 1' />;
+  if (props.type == 'image') {
+    return <img src={props.path} alt='image' />;
+  }
+  else if (props.type == 'text') {
+    return (
+      <div className='basic'>
+        <h1>Title</h1>
+        <p>{props.data}</p>
+      </div>
+    );
+  }
+  else if (props.type == 'card') {
+    return (
+      <div className='basic' style={{textAlign:'center', backgroundColor: '#fefefeff'}}>
+        <img src={props.path} alt='icon' /> <br />
+        {props.data}
+      </div>
+    );
   }
 }
 
